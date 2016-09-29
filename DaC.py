f5780a95d6443e7ad64ceb8aaab758f3e646331a
Manwholikespie <rdominguez@me.com>
2016-09-29 03:17:49 -0500

    Update DaC.py Added some more error checking, and a new function to search google for relevant wikipedia pages in the case that the wikipedia module fails to locate one. Eventually I'll add in support to address disambiguiation problems either statistically (seeing the field of study your terms relate to) or by manual selection... if I go with the latter though I'll probably have to build a web-interface for that though. Hmm. We'll see.

--- DaC.py
+++ DaC.py
@@ -2,24 +2,46 @@ from bs4 import BeautifulSoup
 import requests
 import string
 import wikipedia
+from google import search
 
 def printAll(listName):
     for item in listName:
-        print item[0] + ": " + item[1]
-        print("\n")
+        print "- " + item[0] + ": " + item[1]
+        print("")
 
 def readTerms():
     # read the terms from the file: terms.txt
     terms = [line.rstrip('\n') for line in open('terms.txt')]
     return terms
 
+def wikiSearch(searchString):
+    urls = []
+    for url in search(searchString + " site:wikipedia.org", stop=10):
+        # we only want three urls.
+        # 'relevant' wikipedia sub-urls have '?sa=X&ved=' in them... filter that
+        if (len(urls) < 3) and ("?sa=X&ved=" not in url):
+            urls.append(url)
+
+    return urls
+
 def searchTerm(term):
     # using the wikipedia module, print out the first 5 sentences of the article
     # introduction / summary.
     try:
-        summary = wikipedia.summary(term, sentences=5)
+        summary = wikipedia.summary(term, sentences=2)
     except:
-        summary = "Definition could not be found."
+        try:
+            newTitle = wikiSearch(term)[0].rsplit("/wiki/",1)[1]
+            print("Couldn't find " + term + "... Checking /wiki/"+newTitle)
+            summary = wikipedia.summary(newTitle, sentences=1)
+        except:
+            try:
+                print("Still didn't work for some reason. Using page's .content")
+                newTitle = wikiSearch(term)[0].rsplit("/wiki/",1)[1]
+                summary = wikipedia.WikipediaPage(newTitle).content.split(".")[0]+"."
+            except:
+                summary = "Definition could not be found."
+
     return (term, summary)
 
 
@@ -33,3 +55,5 @@ for term in terms:
 
 printAll(definitions)
 # ==========================   END MAIN FUNCTION   =============================
+# ===========================   BEGIN DEBUGGING   ==============================
+# ============================   END DEBUGGING   ===============================
